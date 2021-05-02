bc1cfaa990078943700ed9567b898a363b4a8d2b
fernandocfbf <fernandocfbf@al.insper.edu.br>
2021-05-02 13:19:14 -0300

    terminei a página home

--- automation/src/pages/home/index.tsx
+++ automation/src/pages/home/index.tsx
@@ -1,6 +1,12 @@
 import styles from './styles.module.scss'
 import { Header } from '../../components/Header'
 import Image from 'next/image'
+import {
+	AiOutlineLinkedin,
+	AiOutlineInstagram,
+	AiOutlineWhatsApp,
+	AiOutlineGithub
+} from 'react-icons/ai'
 
 export default function Home() {
 	return (
@@ -13,17 +19,36 @@ export default function Home() {
       			</h1>
 
 				<p>
-					Here we do the hard work for you using 
+					Here we do the hard work for you using
 					the best tools on the market.
      			</p>
 				<p>
-					Automate process and speed up your 
+					Automate process and speed up your
 					data collection right now with automation.
 				</p>
 
 				<button>
 					Get Start
       			</button>
+
+				<div className={styles.icons}>
+
+					<a href='https://www.linkedin.com/in/fernando-fincatti-93baab189/' style={{ textDecoration: "none" }}>
+						<AiOutlineLinkedin href='' style={{ fontSize: "2rem" }}></AiOutlineLinkedin>
+					</a>
+
+					<a href='https://www.instagram.com/dfernandob/' style={{ textDecoration: "none" }}>
+						<AiOutlineInstagram style={{ fontSize: "2rem" }}></AiOutlineInstagram>
+					</a>
+					<a href='https://wa.me/5511960361402' style={{ textDecoration: "none" }}>
+						<AiOutlineWhatsApp style={{ fontSize: "2rem" }}></AiOutlineWhatsApp>
+					</a>
+
+				</div>
+				<div className={styles.github}>
+					<AiOutlineGithub ></AiOutlineGithub>
+				</div>
+
 			</div>
 		</div>
 	)
